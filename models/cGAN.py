13fa943c6d35ef6755a65f67e837ee2b2bd5c0f7
Sunyun Lee <sunyunlee@Sunyuns-MacBook-Pro.local>
2021-03-04 05:14:20 -0500

    cGAN comment

--- models/cGAN.py
+++ models/cGAN.py
@@ -8,6 +8,7 @@ class cGAN(Model):
 
     def forward(self, x):
         # TODO: implement 
+        # Use Generator and Discriminator 
         pass
 
     def loss_function(self, x_pred, x_in):
@@ -24,10 +25,6 @@ class Generator(nn.Module):
         # TODO: implement 
         pass
 
-    def loss_function(self, x_pred, x_in):
-        # TODO: implement
-        pass
-
 
 class Discriminator(nn.Module):
     def __init__(self, input_dim, label_dim, hidden_dim):
@@ -36,9 +33,5 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         # TODO: implement 
-        pass
-
-    def loss_function(self, x_pred, x_in):
-        # TODO: implement
         pass
 
