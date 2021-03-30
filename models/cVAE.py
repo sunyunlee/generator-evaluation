df32298821bfb5c1777f85d6f886c61d4206afb5
Sunyun Lee <sunyunlee@Sunyuns-MacBook-Pro.local>
2021-03-30 15:50:39 -0400

    MLP superclass and cVAE added

--- models/cVAE.py
+++ models/cVAE.py
@@ -1,43 +1,21 @@
+from typing import List
 from Model import Model 
+from MLP import MultilayerPerceptron
 
-
-class cVAE(Model):
-    def __init__(self, input_dim, label_dim, hidden_dim, latent_dim):
-        super().__init__()
-        pass 
-
-    def forward(self, x):
-        # TODO: implement 
-        pass
-
-    def loss_function(self, x_pred, x_in):
-        # TODO: implement
-        pass
 
 class Encoder(nn.Module): 
-    def __init__(self, input_dim, label_dim, hidden_dim, latent_dim):
-        super().__init__()
-        pass 
+    def __init__(self, input_dim: int, label_dim: int, hidden_dims: List[int], latent_dim: int) -> None:
+        super().__init__(input_dim + label_dim, hidden_dims, latent_dim)
 
     def forward(self, x):
         # TODO: implement 
-        pass
-
-    def loss_function(self, x_pred, x_in):
-        # TODO: implement
         pass
 
 
 class Decoder(nn.Module):
-    def __init__(self, latent, label_dim, hidden_dim, output_dim):
-        super().__init__()
-        pass 
+    def __init__(self, latent_dim: int, label_dim: int, hidden_dims: List[int], output_dim: int) -> None:
+        super().__init__(latent_dim + label_dim, hidden_dims, output_dim)
 
     def forward(self, x):
         # TODO: implement 
         pass
-
-    def loss_function(self, x_pred, x_in):
-        # TODO: implement
-        pass
-
