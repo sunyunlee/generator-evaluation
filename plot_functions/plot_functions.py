e287c2010a8368e603cc290585ba77e2969c8b24
Sunyun Lee <sunyunlee@Sunyuns-MacBook-Pro.local>
2021-03-31 17:42:18 -0400

    plot_functions updated

--- plot_functions/plot_functions.py
+++ plot_functions/plot_functions.py
@@ -2,12 +2,24 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import umap.umap_ as umap
+from typing import List
+import torch 
 # import seaborn as sns
 
 COLORS = ["red", "pink", "orange", "yellow", "green", "teal", "blue", "purple", "brown", "gray"]
 
 
-def plot_UMAP(generated_data: torch.tensor, labels: np.array, n_classes: int, model: str, path: str) -> None:
+def plot_losses(path: str, model_name: str, train_losses: List[int], test_losses: List[int]) -> None:
+    plt.plot(train_losses, "r")
+    plt.plot(test_losses, "b")
+    plt.ylabel("Losses")
+    plt.xlabel("Epochs")
+    plt.title("Train and Test Losses")
+    plt.legend(["Train loss", "Valid Loss"])
+    plt.savefig(os.path.join(path, "{}_losses.png".format(model_name)))
+
+
+def plot_UMAP(generated_data: torch.tensor, labels: np.array, n_classes: int, model_name: str, path: str) -> None:
     reducer = umap.UMAP(random_state=42)
     embedding = reducer.fit_transform(generated_data)
     labels = np.array(labels).flatten()
@@ -16,6 +28,9 @@ def plot_UMAP(generated_data: torch.tensor, labels: np.array, n_classes: int, mo
     for i in range(n_classes):
         indices = np.where(labels==i)[0]
         plt.scatter(embedding[indices, 0], embedding[indices, 1], s=5, label=i, color=COLORS[i%10])
-    plt.title("{} Generated MNIST Data".format(model), fontsize=18)
+    plt.title("{} Generated MNIST Data".format(model_name), fontsize=18)
     plt.legend(markerscale=2)
-    plt.savefig(os.path.join(path, '{}_UMAP.png'.format(model)))
+    plt.savefig(os.path.join(path, '{}_UMAP.png'.format(model_name)))
+
+
+
--- plot_functions/plot_images.py
+++ plot_functions/plot_images.py
@@ -1,11 +1,19 @@
 import torch 
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 
 
-def plot_collage_MNIST(dataset, row: int, col: int):
-    pass
+def plot_image_collage(path: str, model_name: str, images: torch.tensor, n_rows: int, n_cols: int) -> None:
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(100, 100)) # figsize(20, 20)
 
+    for row in range(n_rows):
+        for col in range(n_cols):
+            axs[row][col].imshow(images[row * n_rows + col], cmap="gray")
+            axs[row][col].axis("off")
+
+    plt.subplots_adjust(wspace=-0.3, hspace=0)
+    plt.savefig(os.path.join(path, "{}_generated_images.png".format(model_name)))
 
 def plot_single_MNIST(X: torch.tensor, y: int) -> None:
     """
