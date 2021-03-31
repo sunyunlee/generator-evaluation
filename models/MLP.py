7d6939f07228b022029e03f61a0cecebd41afe5a
Sunyun Lee <sunyunlee@Sunyuns-MacBook-Pro.local>
2021-03-31 17:41:51 -0400

    updated models

--- models/MLP.py
+++ models/MLP.py
@@ -1,21 +1,21 @@
-from neural_net import NeuralNetwork
+# import torch.nn as nn 
 
 
-class MultilayerPerceptron(NeuralNetwork):
-    def __init__(self, input_dim: int, hidden_dims: list, output_dim: int) -> None:
-        super().__init__()
+# class MultilayerPerceptron(nn.Module):
+#     def __init__(self, input_dim: int, hidden_dims: list, output_dim: int) -> None:
+#         super().__init__()
 
-        self.input = nn.Linear(input_dim, hidden_dim[0])
-        if len(hidden_dim) > 1:
-            self.linears = nn.ModuleList([nn.Linear(hidden_dim[i], hidden_dim[i+1]) for i in range(len(hidden_dim) - 1)])
-        else: 
-            self.linears = []
-        self.output = nn.Linear(hidden_dim[-1], latent_dim)
+#         self.input = nn.Linear(input_dim, hidden_dims[0])
+#         if len(hidden_dims) > 1:
+#             self.linears = nn.ModuleList([nn.Linear(hidden_dims[i], hidden_dims[i+1]) for i in range(len(hidden_dims) - 1)])
+#         else: 
+#             self.linears = []
+#         self.output = nn.Linear(hidden_dims[-1], output_dim)
     
-    def forward(self, x):
-        out = F.relu(self.input(x))
-        for i, l in enumerate(self.linears):
-            out = F.relu(l(out))
-        out = self.output(out)
+#     def forward(self, x):
+#         out = F.relu(self.input(x))
+#         for i, l in enumerate(self.linears):
+#             out = F.relu(l(out))
+#         out = self.output(out)
         
-        return out
+#         return out
--- models/cGAN.py
+++ models/cGAN.py
@@ -1,28 +1,12 @@
 from Model import Model 
 
 
-class cGAN(Model): 
-    def __init__(self, input_dim, label_dim, hidden_dim, latent_dim):
-        super().__init__()
-        pass 
-
-    def forward(self, x):
-        # TODO: implement 
-        # Use Generator and Discriminator 
-        pass
-
-    def loss_function(self, x_pred, x_in):
-        # TODO: implement
-        pass
-
-
 class Generator(nn.Module): 
     def __init__(self, latent_dim, label_dim, hidden_dim, output_dim):
         super().__init__()
         pass 
-
-    def forward(self, x):
-        # TODO: implement 
+    
+    def forward(self, z, c):
         pass
 
 
@@ -31,7 +15,6 @@ class Discriminator(nn.Module):
         super().__init__()
         pass 
 
-    def forward(self, x):
-        # TODO: implement 
+    def forward(self, x, c):
         pass
 
