3606c9249db259059a64df5723c62b23dec54caf
AlexHeyman <abheyman23@sbcglobal.net>
2021-04-19 20:58:16 -0400

    Updated cGAN to accept a list of hidden layer dimensions

--- models/cGAN.py
+++ models/cGAN.py
@@ -1,3 +1,7 @@
+"""
+# Some code copied from https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/cgan/cgan.py
+"""
+from typing import List
 from Model import Model
 import torch
 import torch.nn as nn
@@ -5,56 +9,59 @@ import torch.nn as nn
 
 class Generator(nn.Module):
     
-    def __init__(self, latent_dim, label_dim, hidden_dim, output_h, output_w):
+    def __init__(self, latent_dim, label_dim, hidden_dims: List[int],
+                 output_h, output_w):
         super(Generator, self).__init__()
 
-        self.label_embedding = nn.Embedding(label_dim, label_dim)
         self.output_h = output_h
         self.output_w = output_w
         
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
+        def block(in_features, out_features, normalize=True):
+            layers = [nn.Linear(in_features, out_features)]
             if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+                layers.append(nn.BatchNorm1d(out_features, 0.8))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
 
+        hidden_block_layers = []
+        for i in range(len(hidden_dims) - 1):
+            hidden_block_layers.extend(block(hidden_dims[i], hidden_dims[i + 1]))
+        
         self.model = nn.Sequential(
-            *block(latent_dim + label_dim, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, output_h * output_w),
+            *block(latent_dim + label_dim, hidden_dims[0], normalize=False),
+            *hidden_block_layers,
+            nn.Linear(hidden_dims[-1], output_h * output_w),
             nn.Tanh()
         )
 
     def forward(self, noise, labels):
-        gen_input = torch.cat((self.label_embedding(labels), noise), -1)
+        gen_input = torch.cat((labels, noise), -1)
         img = self.model(gen_input)
-        img = img.view(img.shape[0], output_h, output_w)
-        return img
+        return img.view(img.shape[0], output_h, output_w)
 
 
 class Discriminator(nn.Module):
     
-    def __init__(self, input_h, input_w, label_dim, hidden_dim):
+    def __init__(self, input_h, input_w, label_dim, hidden_dims: List[int]):
         super(Discriminator, self).__init__()
 
-        self.label_embedding = nn.Embedding(label_dim, label_dim)
+        def block(in_features, out_features, dropout=True):
+            layers = [nn.Linear(in_features, out_features)]
+            if dropout:
+                layers.append(nn.Dropout(0.4))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
 
+        hidden_block_layers = []
+        for i in range(len(hidden_dims) - 1):
+            hidden_block_layers.extend(block(hidden_dims[i], hidden_dims[i + 1]))
+        
         self.model = nn.Sequential(
-            nn.Linear(input_h * input_w + label_dim, 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 1),
+            *block(input_h * input_w + label_dim, hidden_dims[0], dropout=False),
+            *hidden_block_layers,
+            nn.Linear(hidden_dims[-1], 1)
         )
 
     def forward(self, img, labels):
-        d_in = torch.cat((img.view(img.shape[0], -1), self.label_embedding(labels)), -1)
-        validity = self.model(d_in)
-        return validity
+        disc_input = torch.cat((img.view(img.shape[0], -1), labels), -1)
+        return self.model(disc_input)
